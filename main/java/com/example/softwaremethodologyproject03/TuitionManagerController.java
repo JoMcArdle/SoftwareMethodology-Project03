e7757adc57397873a1cc6692209f645332d7cdce
Yovanny <yovanny.moscoso14@gmail.com>
2023-03-04 13:16:00 -0500

    configuration problem solved

--- main/java/com/example/softwaremethodologyproject03/TuitionManagerController.java
+++ main/java/com/example/softwaremethodologyproject03/TuitionManagerController.java
@@ -35,4 +35,8 @@ public class TuitionManagerController {
         this.rbCt.setDisable(true);
         this.cbStudyAbroad.setDisable(true);
     }
+    @FXML
+    void onCLiclickBurront(){
+
+    }
 }
